7531decd2f3c77685d690c424546ebc31cf3aadf
Longfei Li <lf97310@gmail.com>
2019-12-10 17:31:42 +0800

    add login

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,24 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World !!!")
+
+
+def login(request):
+    result = {}
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        result['success'] = True
+    else:
+        result['success'] = False
+    return HttpResponse(json.dumps(result), content_type="application/json")
+
+
